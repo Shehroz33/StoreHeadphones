7e855ac429cd5bc3fa5f17090c92411ebf43a7af
shehrozamjad <shehrozamjad5000@gmail.com>
2024-05-02 14:28:41 +0500

    well

---
+++ src/Cartitems.jsx
@@ -0,0 +1,80 @@
+import item1 from "./assets/earbuds-prod-1.webp";
+import item2 from "./assets/earbuds-prod-2.webp";
+import item3 from "./assets/earbuds-prod-3.webp";
+import item4 from "./assets/earbuds-prod-4.webp";
+import item5 from "./assets/earbuds-prod-5.webp";
+import item6 from "./assets/headphone-prod-5.webp";
+import item7 from "./assets/headphone-prod-1.webp";
+import item8 from "./assets/headphone-prod-2.webp";
+import item9 from "./assets/headphone-prod-3.webp";
+import item10 from "./assets/headphone-prod-4.webp";
+import item11 from "./assets/headphone-prod-6.webp";
+export const items = [
+    {
+        id : 1,
+        image: item1,
+        name: "Asus Earbuds",
+        price: 2599,
+    },
+    {
+        id : 2,
+        image: item2,
+        name: "Super Earbuds",
+        price: 2599,
+    },
+    {
+        id : 3,
+        image: item3,
+        name: "Boat Earbuds",
+        price: 2599,
+    },
+    {
+        id : 4,
+        image: item4,
+        name: "Alpha Earbuds",
+        price: 2599,
+    },
+    {
+        id : 5,
+        image: item5,
+        name: "Boat Gaming Earbuds",
+        price: 2599,
+    },
+    {
+        id : 6,
+        image: item6,
+        name: "Berka Doodle Headphone",
+        price: 2599,
+    },
+    {
+        id : 7,
+        image: item7,
+        name: "Captain Marvel Headphone",
+        price: 2599,
+    },
+    {
+        id : 8,
+        image: item8,
+        name: "Gaming Headphone",
+        price: 2599,
+    },
+    {
+        id : 9,
+        image: item9,
+        name: "Boat Gaming Headsets",
+        price: 2599,
+    },
+    {
+        id : 10,
+        image: item10,
+        name: "Boat Regular Headphones",
+        price: 2599,
+    },
+    {
+        id : 11,
+        image: item11,
+        name: "Superman Headsets",
+        price: 2599,
+    },
+
+]
